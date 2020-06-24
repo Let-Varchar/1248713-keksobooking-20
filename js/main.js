64f107c5a208f754b5728a6b901b7788ec6d891e
Andrey <ya.5173534@yandex.ru>
2020-06-24 12:08:11 +0300

    метки на карте

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 "use strict";
+
 var map = document.querySelector(".map");
+
 var COUNT_OFFERS = 8;
 var LOCATION_X_MIN = 0;
 var LOCATION_X_MAX = map.clientWidth;
@@ -31,6 +33,15 @@ var BOOKING_PHOTOS = [
 ];
 var BOOKING_TITLE = "Заголовок предложения ";
 var BOOKING_DESCRIPTION = "Описание предложения ";
+var ROOMS_AND_GUESTS = {
+  any: ["any"],
+  1: ["1", "2"],
+  2: ["any", "2"],
+  3: ["0", "1", "2"],
+};
+
+var rooms = document.querySelector("#housing-rooms");
+var guests = document.querySelector("#housing-guests").options;
 var cardTemplate = document
   .querySelector("#card")
   .content.querySelector(".map__card");
@@ -38,20 +49,24 @@ var mainPin = document.querySelector(".map__pins");
 var pinTemplate = document
   .querySelector("#pin")
   .content.querySelector(".map__pin");
-
+var xbh = 2;
 //  делаем поля и карту неактивными
 var addForm = document.querySelector(".ad-form");
 var mainButton = document.querySelector(".map__pin--main");
 var mapFilters = document.querySelector(".map__filters");
-mapFilters.classList.add("ad-form--disabled");
-addForm.classList.add("ad-form--disabled");
-var fieldsets = document.querySelectorAll("fieldset");
 
-for (var i = 0; i < fieldsets.length; i++) {
-  fieldsets[i].setAttribute("disabled", "true");
-}
+//  функция дезактивации карты
+var offActivePage = function () {
+  var fieldsets = document.querySelectorAll("fieldset");
 
-//  функция, для активации полей и карты
+  for (var i = 0; i < fieldsets.length; i++) {
+    fieldsets[i].setAttribute("disabled", "true");
+  }
+
+  mapFilters.classList.add("ad-form--disabled");
+  addForm.classList.add("ad-form--disabled");
+};
+offActivePage();
 
 //получаем случайное число
 var getRandomNumber = function (min, max) {
@@ -104,10 +119,7 @@ var getBookings = function () {
   return bookings;
 };
 
-var pinRender = function () {
-  var pinTemplate = document
-    .querySelector("#pin")
-    .content.querySelector(".map__pin");
+var renderPins = function () {
   var bookingArray = getBookings();
   var pinElem = pinTemplate.cloneNode(true);
   var moveX = PIN_WIDTH / 2;
@@ -124,7 +136,7 @@ var pinRender = function () {
 
   map.appendChild(fragment);
 };
-pinRender();
+renderPins();
 
 //  функция активация карты
 var onActivePage = function () {
@@ -154,31 +166,22 @@ document.addEventListener("keydown", function (evt) {
 });
 
 //  валидация полей по конмате и гостям
-var ROOMS_AND_GUESTS = {
-  any: ["any"],
-  1: ["1", "2"],
-  2: ["any", "2"],
-  3: ["0", "1", "2"],
-};
-
-var ROOMS = document.querySelector("#housing-rooms");
-var GUESTS = document.querySelector("#housing-guests").options;
 
 function handleChangeRoom(evt) {
   var selectedRoomValue = evt.target.value;
   var validGuestValues = ROOMS_AND_GUESTS[selectedRoomValue];
 
-  for (var i = 0; i < GUESTS.length; i++) {
-    var currentRoomValue = GUESTS[i].value;
+  for (var i = 0; i < guests.length; i++) {
+    var currentRoomValue = guests[i].value;
 
     if (validGuestValues.includes(currentRoomValue)) {
-      GUESTS[i].removeAttribute("disabled");
-      GUESTS[i].setAttribute("selected", "");
+      guests[i].removeAttribute("disabled");
+      guests[i].setAttribute("selected", "");
     } else {
-      GUESTS[i].setAttribute("disabled", "");
-      GUESTS[i].removeAttribute("selected");
+      guests[i].setAttribute("disabled", "");
+      guests[i].removeAttribute("selected");
     }
   }
 }
 
-ROOMS.addEventListener("change", handleChangeRoom);
+rooms.addEventListener("change", handleChangeRoom);
